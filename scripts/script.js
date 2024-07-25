00de0cf30af92ca5759dd55befff7cd7f6dd8700
mohammad reza <mrghkh12@gmail.com>
2024-07-25 16:51:36 +0330

    feat(script.js): create voice list & put in selector elem option

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,17 @@ playBtn.addEventListener('click', () => {
     utterance.text = textInput.value
     window.speechSynthesis.speak(utterance)
 })
+
+let voicesList = []
+window.speechSynthesis.onvoiceschanged = () => {
+    voicesList = window.speechSynthesis.getVoices()
+    utterance.voice = voicesList[0]
+    
+
+    voicesList.forEach((voice , i) => {
+        selectVoiceElem.options[i] = new Option(voice.name, i)
+    })
+}
+selectVoiceElem.addEventListener('change', () => {
+    utterance.voice = voicesList[selectVoiceElem.value]
+})
