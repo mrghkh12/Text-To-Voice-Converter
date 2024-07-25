0bcd901a8f88e8194df6f49e7d6d3d21301abf35
mohammad reza <mrghkh12@gmail.com>
2024-07-25 17:01:57 +0330

    style: clean codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,26 @@
-let $ = document
+let $ = document;
 
-let textInput = $.querySelector('.textInput')
+let textInput = $.querySelector(".textInput");
 
-let playBtn = $.querySelector('.option button')
-let selectVoiceElem = $.querySelector('.option select')
+let playBtn = $.querySelector(".option button");
+let selectVoiceElem = $.querySelector(".option select");
 
-let utterance = new SpeechSynthesisUtterance()
+let utterance = new SpeechSynthesisUtterance();
 
-playBtn.addEventListener('click', () => {
-    utterance.text = textInput.value
-    window.speechSynthesis.speak(utterance)
-})
+playBtn.addEventListener("click", () => {
+  utterance.text = textInput.value;
+  window.speechSynthesis.speak(utterance);
+});
 
-let voicesList = []
+let voicesList = [];
 window.speechSynthesis.onvoiceschanged = () => {
-    voicesList = window.speechSynthesis.getVoices()
-    utterance.voice = voicesList[0]
-    
+  voicesList = window.speechSynthesis.getVoices();
+  utterance.voice = voicesList[0];
 
-    voicesList.forEach((voice , i) => {
-        selectVoiceElem.options[i] = new Option(voice.name, i)
-    })
-}
-selectVoiceElem.addEventListener('change', () => {
-    utterance.voice = voicesList[selectVoiceElem.value]
-})
+  voicesList.forEach((voice, i) => {
+    selectVoiceElem.options[i] = new Option(voice.name, i);
+  });
+};
+selectVoiceElem.addEventListener("change", () => {
+  utterance.voice = voicesList[selectVoiceElem.value];
+});
