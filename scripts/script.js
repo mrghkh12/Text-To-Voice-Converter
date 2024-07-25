287b2215a26b8a7fd27824894e75f3ccf942b7e1
mohammad reza <mrghkh12@gmail.com>
2024-07-25 16:40:01 +0330

    feat(script.js): add event play voice

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,8 @@ let playBtn = $.querySelector('.option button')
 let selectVoiceElem = $.querySelector('.option select')
 
 let utterance = new SpeechSynthesisUtterance()
+
+playBtn.addEventListener('click', () => {
+    utterance.text = textInput.value
+    window.speechSynthesis.speak(utterance)
+})
