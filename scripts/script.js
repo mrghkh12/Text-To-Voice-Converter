8e7f122775a7d6391d02b64e03f1d5818e2fc0e0
mohammad reza <mrghkh12@gmail.com>
2024-07-25 16:34:46 +0330

    feat(script.js): select elem in js & requct Web Speech API constructor

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let $ = document
+
+let textInput = $.querySelector('.textInput')
+
+let playBtn = $.querySelector('.option button')
+let selectVoiceElem = $.querySelector('.option select')
+
+let utterance = new SpeechSynthesisUtterance()
